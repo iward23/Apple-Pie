c37e0b8c1c0db85656ab94caa722091c9749298d
Imani Ward <idward712@gmail.com>
2022-10-18 14:19:13 -0400

    o

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+    let incorrectMovesAllowed = 7
+    
     @IBOutlet var TreeImageView: UIImageView!
     @IBOutlet var correctWordLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
-    
-    
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
